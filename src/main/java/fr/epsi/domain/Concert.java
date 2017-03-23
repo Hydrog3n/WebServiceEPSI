860eeda41678b3a775b7e8ac43e885e71d53d4a3
charroux <benoit.charroux@free.fr>
2017-03-23 17:01:00 +0100

    debut web service

---
+++ src/main/java/fr/epsi/domain/Concert.java
@@ -0,0 +1,5 @@
+package fr.epsi.domain;
+
+public class Concert {
+
+}
---
+++ src/main/java/fr/epsi/web/WebService.java
@@ -0,0 +1,33 @@
+package fr.epsi.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fr.epsi.domain.Concert;
+import fr.epsi.domain.Reservation;
+
+@Controller
+public class WebService {
+	
+	@RequestMapping(method=RequestMethod.GET, value="/concert")
+	@ResponseBody
+	public List<Concert> listerConcert(){
+		System.out.println("méthode: listerConcert");
+		return new ArrayList<Concert>();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/concert/{idConcert}")
+	@ResponseBody
+	public int choisirPlace(@PathVariable(name="idConcert") int id, @RequestBody Reservation reservation){
+		System.out.println("méthode: choisirPlace, concert: " + id + ", reservation: " + reservation);
+		return 0;
+	}
+
+}
