6971d1370347aeaac055a67881bcc7029e4ade68
charroux <benoit.charroux@free.fr>
2017-03-23 15:25:34 +0100

    first commit

---
+++ src/main/java/fr/epsi/WebServiceEpsiApplication.java
@@ -0,0 +1,12 @@
+package fr.epsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceEpsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceEpsiApplication.class, args);
+	}
+}
